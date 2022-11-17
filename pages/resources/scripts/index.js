fcc399ec73f1aa783bc6073d3a8df102fe8e7234
AlaziSera <107665627+J-R-Fisher@users.noreply.github.com>
2022-11-17 10:17:23 -0500

    Update index.js

--- pages/resources/scripts/index.js
+++ pages/resources/scripts/index.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', evt => {
+    var but = document.createElement('button');
     
+    but.onclick = () => {
+        console.log('BUTTON PRESSED');
+    };
+
+    but.innerHTML = 'HELLO';
+    document.appendChild(but);
 });
